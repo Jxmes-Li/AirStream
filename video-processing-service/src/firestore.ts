e1ac416d249db4b5729ecedfce4388a260930c56
Jxmes-Li <123266816+Jxmes-Li@users.noreply.github.com>
2024-04-26 21:44:01 -0400

    finished db firestore logic

--- video-processing-service/src/firestore.ts
+++ video-processing-service/src/firestore.ts
@@ -14,3 +14,31 @@ const firestore = new Firestore();
       ssl: false
   });
 } */
+
+const videoCollectionId = 'videos';
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: 'processing' | 'processed',
+  title?: string,
+  description?: string
+}
+
+async function getVideo(videoId: string) {
+  const snapshot = await firestore.collection(videoCollectionId).doc(videoId).get();
+  return (snapshot.data() as Video) ?? {}
+}
+
+export function setVideo(videoId: string, video: Video) {
+  return firestore
+    .collection(videoCollectionId)
+    .doc(videoId)
+    .set(video, {merge: true})
+}
+
+export async function isVideoNew(videoId: string) {
+  const video = await getVideo(videoId);
+  return video?.status === undefined;
+}
